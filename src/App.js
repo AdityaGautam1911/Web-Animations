290f12c04ae60a5891bfd25d2093b1b8961063ab
AdityaGautam1911 <adityagautam1911@gmail.com>
2024-09-28 12:00:27 +0530

    initial animations

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Lottie from "lottie-react";
+import animationData from "./phone.json";
+import animationData1 from "./Name.json";
+import animationData2 from "./bg.json";
+
+import ResponsiveAppBar from "./appBar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ResponsiveAppBar />
+      <div className="App">
+        {/* Background Animation */}
+        <Lottie
+          animationData={animationData2}
+          className="background-animation"
+        />
+
+        {/* Foreground Animations */}
+        <Lottie
+          animationData={animationData1}
+          className="foreground-animation"
+          resizeMode="center"
+          style={{ width: 300, marginBottom: 0, paddingBottom: 0 }}
+        />
+        <Lottie
+          animationData={animationData}
+          className="foreground-animation"
+          resizeMode="center"
+          style={{ width: 300, marginTop: 0, paddingTop: 0 }}
+        />
+      </div>
     </div>
   );
 }
